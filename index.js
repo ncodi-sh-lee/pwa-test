148f628f1d3747034e7eeda35bb882b731926a7d
ncodi <sh.lee@ncodi.co.kr>
2019-05-17 16:13:32 +0900

    __dirname

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT;
 const path = require("path");
 
 app.get('/', function(req, res) { 
-    res.sendFile(path.join('./index.html')); 
+    res.sendFile(path.join(__dirname+'./index.html')); 
 });
 
 app.listen(PORT);
