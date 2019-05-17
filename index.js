f629f871abaa404a80dbd006614eea36ff6b7d5b
ncodi <sh.lee@ncodi.co.kr>
2019-05-17 16:02:43 +0900

    path /index.html

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
+const path = require("path");
 
 app.get('/', function(req, res) { 
-    res.send('index.html'); 
+    res.sendFile(path.join('/index.html')); 
 });
 
 app.listen(PORT);
