390909cce62a34d9ebd92e3d77bcb96aa1c2d4cd
ncodi <sh.lee@ncodi.co.kr>
2019-05-20 10:22:59 +0900

    index.js 변경

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
-const router = require('./router/app')(app);
+//const router = require('./router/app')(app);
 const path = require("path");
 
 app.get('/', function(req, res) { 
     res.sendFile(path.join(__dirname+'/index.html')); 
 });
-
-app.listen(PORT);
 app.use(express.static('public'));
+app.listen(PORT);
